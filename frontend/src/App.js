ba331202b6b546357c1f5b5945fe8268c124c2b5
crjone24 <crjone24@ncsu.edu>
2025-04-12 12:04:28 -0400

    Updated the profile page to have multiple profile pages.
    Added a dropdown on the sidebar to select a profile. Added a line at the top of the profile page to show the current profile, edit it's name, and set a profile as the default.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,164 +1,308 @@
-import './static/App.css';
+import "./static/App.css";
 
-import React from 'react';
-import Sidebar from './sidebar/Sidebar';
-import ApplicationPage from './application/ApplicationPage';
-import SearchPage from './search/SearchPage';
-import LoginPage from './login/LoginPage';
-import ManageResumePage from './resume/ManageResumePage';
-import ProfilePage from './profile/ProfilePage';
-import axios from 'axios';
-import MatchesPage from './matches/MatchesPage';
+import React from "react";
+import Sidebar from "./sidebar/Sidebar";
+import ApplicationPage from "./application/ApplicationPage";
+import SearchPage from "./search/SearchPage";
+import LoginPage from "./login/LoginPage";
+import ManageResumePage from "./resume/ManageResumePage";
+import ProfilePage from "./profile/ProfilePage";
+import axios from "axios";
+import MatchesPage from "./matches/MatchesPage";
+import { CONSTANTS } from "./data/Constants";
 
 export default class App extends React.Component {
-	constructor(props) {
-		super(props);
-		let mapRouter = {
-			SearchPage: <SearchPage />,
-			ApplicationPage: <ApplicationPage />,
-			LoginPage: <LoginPage />,
-			ManageResumePage: <ManageResumePage />,
-			ProfilePage: <ProfilePage />,
-			MatchesPage: <MatchesPage />
-		};
-		this.state = {
-			currentPage: <LoginPage />,
-			mapRouter: mapRouter,
-			sidebar: false,
-			userProfile: null,
-			showLogoutModal: false // ✅ State to control the custom modal
-		};
-		this.sidebarHandler = this.sidebarHandler.bind(this);
-		this.updateProfile = this.updateProfile.bind(this);
-	}
+  constructor(props) {
+    super(props);
 
-	updateProfile = (profile) => {
-		this.setState({
-			userProfile: profile,
-			currentPage: <ProfilePage profile={profile} updateProfile={this.updateProfile} />
-		});
-	};
+    this.state = {
+      currentPage: CONSTANTS.PAGES.LOGIN.NAME,
+      sidebar: false,
+      userProfile: null,
+      profilesList: [],
+      defaultProfile: 0,
+      showLogoutModal: false, // ✅ State to control the custom modal
+    };
+    this.sidebarHandler = this.sidebarHandler.bind(this);
+    this.updateProfile = this.updateProfile.bind(this);
+    this.onProfileSelect = this.onProfileSelect.bind(this);
+    this.createNewProfile = this.createNewProfile.bind(this);
+    this.setDefaultProfile = this.setDefaultProfile.bind(this);
+  }
 
-	async componentDidMount() {
-		if (localStorage.getItem('token')) {
-			const userId = localStorage.getItem('userId');
-			await axios
-				.get('http://localhost:5000/getProfile', {
-					headers: {
-						userid: userId,
-						Authorization: `Bearer ${localStorage.getItem('token')}`
-					}
-				})
-				.then((res) => {
-					this.sidebarHandler(res.data);
-				})
-				.catch((err) => console.log(err.message));
-		}
-	}
+  updateProfile = (body, name = "") => {
+    axios
+      .post("http://localhost:5000/updateProfile", body, {
+        headers: {
+          userid: localStorage.getItem("userId"),
+          Authorization: `Bearer ${localStorage.getItem("userId")}`,
+        },
+      })
+      .then((res) => {
+        let profile = { ...this.state.userProfile, ...body };
+        this.setState({
+          userProfile: profile,
+          currentPage: CONSTANTS.PAGES.PROFILE.NAME,
+        });
 
-	sidebarHandler = (user) => {
-		this.setState({
-			currentPage: (
-				<ProfilePage profile={user} updateProfile={this.updateProfile.bind(this)} />
-			),
-			sidebar: true,
-			userProfile: user
-		});
-	};
+        if (name) {
+          const index = this.state.profilesList.findIndex(
+            (prof) =>
+              prof[CONSTANTS.PROFILE.ID] ===
+              this.state.userProfile[CONSTANTS.PROFILE.ID]
+          );
+          let updatedProfilesList = [...this.state.profilesList]; // important to create a copy, otherwise you'll modify state outside of setState call
+          updatedProfilesList[index][CONSTANTS.PROFILE.PROFILE_NAME] = name;
+          this.setState({
+            profilesList: updatedProfilesList,
+          });
+        }
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
 
-	// ✅ Show Logout Modal
-	handleLogout = () => {
-		this.setState({ showLogoutModal: true });
-	};
+  createNewProfile = async (name) => {
+    axios
+      .post(
+        "http://localhost:5000/createProfile",
+        { [CONSTANTS.PROFILE.PROFILE_NAME]: name },
+        {
+          headers: {
+            userid: localStorage.getItem("userId"),
+            Authorization: `Bearer ${localStorage.getItem("userId")}`,
+          },
+        }
+      )
+      .then((res) => {
+        let newProfile = {
+          [CONSTANTS.PROFILE.ID]: res.data[CONSTANTS.PROFILE.ID],
+          [CONSTANTS.PROFILE.PROFILE_NAME]: name,
+          [CONSTANTS.PROFILE.IS_DEFAULT]: false,
+        };
+        this.setState({
+          profilesList: [...this.state.profilesList, newProfile],
+        });
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
 
-	// ✅ Confirm Logout
-	confirmLogout = () => {
-		localStorage.removeItem('token');
-		localStorage.removeItem('userId');
-		this.setState({
-			sidebar: false,
-			showLogoutModal: false
-		});
-	};
+  /**
+   * Uses the chosen profile id to witch the current profile being viewed.
+   * @param {React.ChangeEvent} event
+   */
+  onProfileSelect = async (event) => {
+    const userId = localStorage.getItem("userId");
+    await axios
+      .get("http://localhost:5000/getProfile/" + event, {
+        headers: {
+          userid: userId,
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => {
+        this.setState({
+          userProfile: res.data,
+        });
+      })
+      .catch((err) => console.log(err.message));
+  };
 
-	// ✅ Cancel Logout
-	cancelLogout = () => {
-		this.setState({ showLogoutModal: false });
-	};
+  /**
+   * Make the current profile the default profile.
+   */
+  setDefaultProfile = async () => {
+    const userId = localStorage.getItem("userId");
 
-	switchPage(pageName) {
-		const currentPage =
-			pageName === 'ProfilePage' ? (
-				<ProfilePage
-					profile={this.state.userProfile}
-					updateProfile={this.updateProfile.bind(this)}
-				/>
-			) : (
-				this.state.mapRouter[pageName]
-			);
-		this.setState({
-			currentPage: currentPage
-		});
-	}
+    // If the current profile is already the default, don't do anything
+    let profileId = this.state.userProfile[CONSTANTS.PROFILE.ID];
+    if (this.state.defaultProfile === profileId) return;
 
-	render() {
-		const { showLogoutModal } = this.state;
+    // Set Default proile
+    await axios
+      .post(
+        `http://localhost:5000/setDefaultProfile/${profileId}`,
+        {},
+        {
+          headers: {
+            userid: userId,
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      )
+      .then((res) => {
+        this.setState({
+          defaultProfile: res.data[CONSTANTS.PROFILE.DEFAULT_PROFILE],
+        });
+      })
+      .catch((err) => console.log(err.message));
+  };
 
-		return (
-			<div>
-				{this.state.sidebar ? (
-					<div className='main-page'>
-						<Sidebar
-							switchPage={this.switchPage.bind(this)}
-							handleLogout={this.handleLogout}
-						/>
-						<div className='main'>
-							<div className='container'>
-								<h1 className='text-center' style={{ marginTop: '2%', fontWeight: '300'}}>
-									Application Tracking System
-								</h1>
-								{this.state.currentPage}
-							</div>
-						</div>
-					</div>
-				) : (
-					<div className='main'>
-						<div className='content'>
-							<h1
-								className='text-center'
-								style={{
-									marginTop: 30,
-									padding: '0.4em',
-									fontWeight: '300'
-								}}
-							>
-								Application Tracking System
-							</h1>
-							<LoginPage side={this.sidebarHandler} />
-						</div>
-					</div>
-				)}
+  async componentDidMount() {
+    if (localStorage.getItem("token")) {
+      const userId = localStorage.getItem("userId");
+      await axios
+        .get("http://localhost:5000/getProfile", {
+          headers: {
+            userid: userId,
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        })
+        .then((res) => {
+          this.sidebarHandler(res.data);
+        })
+        .catch((err) => console.log(err.message));
 
-				{/* ✅ Custom Logout Modal */}
-				{showLogoutModal && (
-					<div className='modal-overlay'>
-						<div className='custom-modal'>
-							<h2>Confirm Logout</h2>
-							<p>Are you sure you want to logout?</p>
-							<div className='modal-buttons'>
-								<button className='btn cancel-btn' onClick={this.cancelLogout}>
-									Cancel
-								</button>
-								<button className='btn logout-btn' onClick={this.confirmLogout}>
-									Logout
-								</button>
-							</div>
-						</div>
-					</div>
-				)}
+      await axios
+        .get("http://localhost:5000/getProfileList", {
+          headers: {
+            userid: userId,
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        })
+        .then((res) => {
+          this.setState({
+            profilesList: res.data[CONSTANTS.PROFILE.PROFILE_LIST],
+            defaultProfile: res.data[CONSTANTS.PROFILE.DEFAULT_PROFILE],
+          });
+        })
+        .catch((err) => console.log(err.message));
+    }
+  }
 
-				{/* ✅ Modal Styling */}
-				<style>{`
+  sidebarHandler = (user) => {
+    this.setState({
+      currentPage: CONSTANTS.PAGES.PROFILE.NAME,
+      sidebar: true,
+      userProfile: user,
+    });
+  };
+
+  // ✅ Show Logout Modal
+  handleLogout = () => {
+    this.setState({ showLogoutModal: true });
+  };
+
+  // ✅ Confirm Logout
+  confirmLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    this.setState({
+      sidebar: false,
+      showLogoutModal: false,
+    });
+  };
+
+  // ✅ Cancel Logout
+  cancelLogout = () => {
+    this.setState({ showLogoutModal: false });
+  };
+
+  switchPage(pageName) {
+    this.setState({
+      currentPage: pageName,
+    });
+  }
+
+  getCurrentPage(pageName) {
+    switch (pageName) {
+      case CONSTANTS.PAGES.SEARCH.NAME:
+        return <SearchPage />;
+      case CONSTANTS.PAGES.APPLICATION.NAME:
+        return <ApplicationPage />;
+      case CONSTANTS.PAGES.LOGIN.NAME:
+        return <LoginPage />;
+      case CONSTANTS.PAGES.MANAGE_RESUME.NAME:
+        return <ManageResumePage />;
+      case CONSTANTS.PAGES.MATCHES.NAME:
+        return <MatchesPage profile={this.state.userProfile} />;
+      default:
+        return (
+          <ProfilePage
+            profile={this.state.userProfile}
+            updateProfile={this.updateProfile.bind(this)}
+            setDefaultProfile={this.setDefaultProfile}
+            isDefault={
+              this.state.profilesList.length > 0
+                ? this.state.userProfile[CONSTANTS.PROFILE.ID] ===
+                  this.state.defaultProfile
+                : false
+            }
+          />
+        );
+    }
+  }
+
+  render() {
+    const { showLogoutModal } = this.state;
+
+    return (
+      <div>
+        {this.state.sidebar ? (
+          <div className="main-page">
+            <Sidebar
+              switchPage={this.switchPage.bind(this)}
+              handleLogout={this.handleLogout}
+              onProfileSelect={this.onProfileSelect}
+              profilesList={this.state.profilesList}
+              defaultProfile={this.state.defaultProfile}
+              userProfile={this.state.userProfile}
+              createNewProfile={this.createNewProfile}
+              currentPage={this.state.currentPage}
+            />
+            <div className="main">
+              <div className="container">
+                <h1
+                  className="text-center"
+                  style={{ marginTop: "2%", fontWeight: "300" }}
+                >
+                  Application Tracking System
+                </h1>
+                {this.getCurrentPage(this.state.currentPage)}
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="main">
+            <div className="content">
+              <h1
+                className="text-center"
+                style={{
+                  marginTop: 30,
+                  padding: "0.4em",
+                  fontWeight: "300",
+                }}
+              >
+                Application Tracking System
+              </h1>
+              <LoginPage side={this.sidebarHandler} />
+            </div>
+          </div>
+        )}
+
+        {/* ✅ Custom Logout Modal */}
+        {showLogoutModal && (
+          <div className="modal-overlay">
+            <div className="custom-modal">
+              <h2>Confirm Logout</h2>
+              <p>Are you sure you want to logout?</p>
+              <div className="modal-buttons">
+                <button className="btn cancel-btn" onClick={this.cancelLogout}>
+                  Cancel
+                </button>
+                <button className="btn logout-btn" onClick={this.confirmLogout}>
+                  Logout
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {/* ✅ Modal Styling */}
+        <style>{`
 					.modal-overlay {
 						position: fixed;
 						top: 0;
@@ -211,7 +355,7 @@ export default class App extends React.Component {
 						opacity: 0.9;
 					}
 				`}</style>
-			</div>
-		);
-	}
+      </div>
+    );
+  }
 }
--- frontend/src/matches/MatchesPage.js
+++ frontend/src/matches/MatchesPage.js
@@ -1,61 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import JobTable from '../search/JobTable';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSyncAlt } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from "react";
+import JobTable from "../search/JobTable";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSyncAlt } from "@fortawesome/free-solid-svg-icons";
 
+const Recommendations = (props) => {
+  const [recommendedJobs, setRecommendedJobs] = useState([]);
+  const [isFetchingJobs, setIsFetchingJobs] = useState(true);
 
-const Recommendations = () => {
-	const [recommendedJobs, setRecommendedJobs] = useState([]);
-	const [isFetchingJobs, setIsFetchingJobs] = useState(true);
+  useEffect(() => {
+    const lastMatchResults = localStorage.getItem("lastMatchResults");
+    const lastMatchTime = localStorage.getItem("lastMatchTime");
+    if (
+      lastMatchResults &&
+      new Date().getTime() - lastMatchTime < 1000 * 60 * 60
+    ) {
+      setRecommendedJobs(JSON.parse(lastMatchResults));
+      setIsFetchingJobs(false);
+    } else {
+      fetchRecommendations();
+    }
+  }, []);
 
-	useEffect(() => {
-    const lastMatchResults = localStorage.getItem('lastMatchResults');
-    const lastMatchTime = localStorage.getItem('lastMatchTime');
-		if (lastMatchResults && new Date().getTime() - lastMatchTime < 1000 * 60 * 60) {
-			setRecommendedJobs(JSON.parse(lastMatchResults));
-			setIsFetchingJobs(false);
-		} else {
-			fetchRecommendations();
-		}
-	}, []);
+  useEffect(() => {
+    const lastMatchTime = localStorage.getItem("lastMatchTime");
+    if (lastMatchTime) {
+      fetchRecommendations();
+    }
+  }, [props.profile]);
 
-	const fetchRecommendations = async () => {
-		try {
-			setIsFetchingJobs(true);
-			const response = await fetch('http://localhost:5000/getRecommendations', {
-				headers: {
-					Authorization: 'Bearer ' + localStorage.getItem('token'),
-					'Access-Control-Allow-Origin': 'http://localhost:3000',
-					'Access-Control-Allow-Credentials': 'true'
-				},
-				method: 'GET'
-			});
-			const data = await response.json();
-			if (data && data['error']) {
-				throw new Error(data['error']);
-			} else {
-				localStorage.setItem('lastMatchTime', new Date().getTime());
-				localStorage.setItem('lastMatchResults', JSON.stringify(data));
-				setRecommendedJobs(data);
-			}
-		} catch (error) {
-			console.log(error.message);
-		} finally {
-			setIsFetchingJobs(false);
-		}
-	};
+  const fetchRecommendations = async () => {
+    try {
+      setIsFetchingJobs(true);
+      const response = await fetch("http://localhost:5000/getRecommendations", {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+          "Access-Control-Allow-Origin": "http://localhost:3000",
+          "Access-Control-Allow-Credentials": "true",
+        },
+        method: "GET",
+      });
+      const data = await response.json();
+      if (data && data["error"]) {
+        throw new Error(data["error"]);
+      } else {
+        localStorage.setItem("lastMatchTime", new Date().getTime());
+        localStorage.setItem("lastMatchResults", JSON.stringify(data));
+        setRecommendedJobs(data);
+      }
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      setIsFetchingJobs(false);
+    }
+  };
 
   return (
-  <div className='container'>
-    <h2 class='d-flex align-items-center justify-content-center mt-4 mb-5'>
-    Recommended Jobs &nbsp;
-    <button className='btn btn-secondary' onClick={fetchRecommendations}>
-      <FontAwesomeIcon icon={faSyncAlt} />
-    </button>
-    </h2>
-      <JobTable rows={recommendedJobs} loading={isFetchingJobs} emptyMessage="Make sure you've filled out your profile." />
-		</div>
-	);
+    <div className="container">
+      <h2 class="d-flex align-items-center justify-content-center mt-4 mb-5">
+        Recommended Jobs &nbsp;
+        <button className="btn btn-secondary" onClick={fetchRecommendations}>
+          <FontAwesomeIcon icon={faSyncAlt} />
+        </button>
+      </h2>
+      <JobTable
+        rows={recommendedJobs}
+        loading={isFetchingJobs}
+        emptyMessage="Make sure you've filled out your profile."
+      />
+    </div>
+  );
 };
 
 export default Recommendations;
