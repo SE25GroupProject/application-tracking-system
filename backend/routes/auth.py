33a8875b0be6112ae39a5fcb76b5b5046888ffcc
crjone24 <crjone24@ncsu.edu>
2025-04-15 00:05:39 -0400

    Removed whitespace

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -105,7 +105,7 @@ def sign_up():
 
         password = data["password"]
         password_hash = hashlib.md5(password.encode())
-    
+
         # Create an empty default profile
         default_profile = Profile(
             profileName=f"{data['fullName']}'s Default",
